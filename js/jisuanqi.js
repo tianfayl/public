04f05331186b424341bc0a609fc199b08f0a040a
tianfayl <admin@tianfayl.cn>
2017-08-10 09:13:26 +0800

    提交文件

---
+++ js/jisuanqi.js
@@ -0,0 +1,22 @@
+var myApp=angular.module("myApp",[]);
+myApp.controller("myController",function($scope){
+	$scope.shuzi="";
+	$scope.jieguo=0;
+	$scope.num=function(num){
+		$scope.shuzi+=num;
+	}
+	$scope.js=function(){
+		$scope.jieguo=(eval($scope.shuzi));
+	}
+	$scope.reversal=function(){
+		if(eval($scope.shuzi)>0){
+			$scope.shuzi="-"+$scope.shuzi;
+		}else{
+			$scope.shuzi=$scope.shuzi.substring(1);
+		}
+	} 
+	$scope.remove=function(){
+		$scope.shuzi="";
+		$scope.jieguo=0;
+	}
+})
